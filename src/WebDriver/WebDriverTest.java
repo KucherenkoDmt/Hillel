3372c1482972d1d66c0268270fef33ec46440309
Dmytrii Kucherenko <kucherenko.dmt@gmail.com>
2018-02-27 19:27:46 +0200

    added steps

--- src/WebDriver/WebDriverTest.java
+++ src/WebDriver/WebDriverTest.java
@@ -2,6 +2,9 @@ package WebDriver;
 
 import HomeWork.Log.ConsoleLogger;
 import HomeWork.Log.Logger;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
 import org.seleniumhq.jetty9.util.log.Log;
 
 import java.io.IOException;
@@ -10,7 +13,8 @@ import static com.sun.webkit.perf.WCFontPerfLogger.log;
 
 public class WebDriverTest {
     public static void main(String[] args) throws IOException {
-
+        WebDriver driver = new ChromeDriver();
+        driver.findElement(By.partialLinkText("New...")).click();
 
     }
 }
--- src/WebDriver/WebdriverHomeWork.java
+++ src/WebDriver/WebdriverHomeWork.java
@@ -4,19 +4,22 @@ import HomeWork.Log.AbstractLogger;
 import HomeWork.Log.ConsoleLogger;
 import HomeWork.Log.Logger;
 import HomeWork.UrlBuilder.Url;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.remote.server.handler.FindElement;
 
 import java.io.IOException;
 
 public class WebdriverHomeWork extends AbstractLogger {
     public static void main(String[] args) throws IOException {
-       WebdriverHomeWork webdriverHomeWork = new WebdriverHomeWork();
-       webdriverHomeWork.homeWork();
+        WebdriverHomeWork webdriverHomeWork = new WebdriverHomeWork();
+        webdriverHomeWork.webDriverhomeWork();
     }
 
-    public void homeWork() throws IOException {
+    public void webDriverhomeWork() throws IOException {
         log("Create url");
         Url urlOfComments = new Url.UrlBuilder("comments.azurewebsites.net").build();
         System.out.println(urlOfComments.getUrl());
@@ -26,6 +29,33 @@ public class WebdriverHomeWork extends AbstractLogger {
 
         log("Navigate to Url ");
         driver.get(urlOfComments.getUrl());
+
+        log("get element \"New...\" and click");
+        driver.findElement(By.xpath("//*[@id='command-navigation']/input[1]")).click();
+
+        log("Add comment text");
+        String comentText = "some text"+(int)Math.random();
+        driver.findElement(By.id("Text")).sendKeys(comentText);
+
+        log("Add original number");
+        int number = (int) (999*Math.random());
+        driver.findElement(By.id("Number")).sendKeys(number+"");
+
+        log("Add 1 category");
+        driver.findElement(By.xpath("//*[@class=\"categoryitem\"]/span[contains(text(),'Cat1')]/../*[@type]")).click();
+        driver.findElement(By.name("CurSelect")).click();
+
+        log("click save");
+        driver.findElement(By.xpath("//*[@id='editor-navigation']/input[1]")).click();
+
+        log("Click Return");
+        driver.findElement(By.linkText("Return")).click();
+
+        log("Check comment");
+
+
+        log("Closing webdriver");
+       // driver.quit();
     }
 
     @Override
