b01dba2c55c15327ec9f83ed4e08d8a34f69e18a
Dmytrii Kucherenko <kucherenko.dmt@gmail.com>
2018-03-09 18:27:26 +0200

    a commit

--- src/WebDriver/WebdriverHomeWork.java
+++ src/WebDriver/WebdriverHomeWork.java
@@ -6,6 +6,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.NoSuchElementException;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 
 import javax.xml.bind.Element;
